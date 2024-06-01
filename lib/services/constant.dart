2c72f9227ddfbe2a0c27e773f97c1e472eb6b12e
Andy Lam <andyl@Andys-Laptop.lan>
2024-05-31 21:32:07 -0400

    added the services folder, not fully functional yet

---
+++ lib/services/constant.dart
@@ -0,0 +1,24 @@
+const baseUrl = "http://localhost:8080";
+
+const postgresql = "/db/postgresql";
+
+const users = "$postgresql/users";
+const doctors = "$postgresql/doctors";
+
+
+// const allLists = "/";
+// const newList = "/lists";
+// const singleList = "$newList/";
+// const items = "/items";
+// const itemsByList = "$items/";
+// const singleItem = "$items/";
+
+// const firebase = "$db/fbase";
+// const mongodb = "$db/mongodb";
+// const cache = "/cache";
+// const redis = "$cache/redis";
+// const auth = "/authentication";
+// const basicAuth = "$auth/basic/";
+// const bearerAuth = "$auth/bearer/";
+// const api = "/api";
+// const restapi = "$api/restapi";
