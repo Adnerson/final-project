e4d813da146063c9941d6de29ae560b3ffafcd6a
Andy Lam <andyl@Andys-Laptop.lan>
2024-06-04 19:02:28 -0400

    cleaned up call_screen code, found a way to solve the problem with the iconbutton but will implement later

--- lib/screens/call_screen.dart
+++ lib/screens/call_screen.dart
@@ -41,29 +41,9 @@ class CallScreenState extends State<CallScreen> with Func {
               ),
             ),
             const SizedBox(height: 8),
-            switchDoctors(),
-            const SizedBox(height: 8),
-            // doctorDisplay(),
             doctorsBuilder(context),
           ],
         ),
-      ),
-    );
-  }
-
-  ElevatedButton switchDoctors() {
-    return ElevatedButton(
-      onPressed: () {
-        setState(() {
-          showPrimaryDoctors = !showPrimaryDoctors;
-        });
-      },
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.blue,
-      ),
-      child: Text(
-        showPrimaryDoctors ? 'Show All Doctors' : 'Show Primary Doctors',
-        style: const TextStyle(color: Colors.white),
       ),
     );
   }
@@ -83,14 +63,17 @@ class CallScreenState extends State<CallScreen> with Func {
                 var entryList = snapshot.data!.toList();
                 return Card(
                   child: ListTile(
-                    title:  Padding(
+                    title: Padding(
                       padding: const EdgeInsets.symmetric(vertical: 5),
                       child: Text(entryList[index]['name'],
                           style: const TextStyle(
                               fontSize: 18, fontWeight: FontWeight.bold)),
                     ),
-                    subtitle: Text('${entryList[index]['specialization']}\n${entryList[index]['phoneNumber']}\n${entryList[index]['address']}'),
+                    subtitle: Text(
+                        '${entryList[index]['specialization']}\n${entryList[index]['phoneNumber']}\n${entryList[index]['address']}'),
                     trailing: IconButton(
+                      //ontap, i am trying to prompt the user into calling/getting directions
+                      //however, I will probably have to create 2 map<int, String> of index and phoneNumber/address
                       onPressed: () {},
                       icon: const Icon(Icons.add_circle_outline),
                       iconSize: 30,
@@ -104,112 +87,6 @@ class CallScreenState extends State<CallScreen> with Func {
             return const Text("error");
           }
         },
-      ),
-    );
-  }
-
-  Expanded doctorDisplay() {
-    return Expanded(
-      child: ListView.builder(
-        physics: const AlwaysScrollableScrollPhysics(),
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        itemCount: getDisplayedDoctors().length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: ListTile(
-              title: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 5),
-                child: Text('aaaa',
-                    style:
-                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-              ),
-              subtitle: const Text('specialty\nnumber\naddress'),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.add_circle_outline),
-                iconSize: 30,
-                color: Colors.blue.withOpacity(0.9),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  Container containerDoctor(Doctor doctor) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            // spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  doctor.getName(),
-                  style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 10),
-                Text(doctor.getSpecialty()),
-                Text(doctor.getNumber()),
-                Text(doctor.getAddress()),
-              ],
-            ),
-          ),
-
-          // plusButton2(doctor),
-        ],
-      ),
-    );
-  }
-
-  GestureDetector plusButton2(Doctor doctor) {
-    return GestureDetector(
-      onTapDown: (TapDownDetails details) {
-        setState(() {
-          hasInteracted = true;
-        });
-      },
-      onTapCancel: () {
-        setState(() {
-          hasInteracted = false;
-        });
-      },
-      onTap: () {
-        setState(() {
-          doctor.togglePrimary();
-          if (doctor.isPrimary) {
-            primaryDoctors.add(doctor);
-          } else {
-            primaryDoctors.remove(doctor);
-          }
-        });
-      },
-      child: Container(
-        width: 30,
-        height: 30,
-        decoration: BoxDecoration(
-          color: doctor.isPrimary ? Colors.blue.withOpacity(0.8) : Colors.blue,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Icon(doctor.isPrimary ? Icons.remove : Icons.add,
-            color: Colors.white),
       ),
     );
   }
